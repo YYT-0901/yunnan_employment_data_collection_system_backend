dce5c7ac14cfc612e18bf13eb982052affcf0580
Zhang Miguel <zhang.warcraft@gmail.com>
2025-11-18 02:09:43 +0800

    feat: add province enterprise status flow and operation logs

--- yunnan-province/src/main/java/com/yunnanprovince/controller/AccountController.java
+++ yunnan-province/src/main/java/com/yunnanprovince/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.yunnanprovince.controller;
 
 import com.yunnancommon.component.RedisComponent;
+import com.yunnancommon.annotation.OperationLog;
 import com.yunnancommon.controller.ABaseController;
 import com.yunnancommon.entity.constants.Constants;
 import com.yunnancommon.entity.dto.ChangeStatusDto;
@@ -93,6 +94,7 @@ public class AccountController extends ABaseController {
      * 新增账号(企业,市)
      * */
     @PostMapping("/createAccount")
+    @OperationLog(module = "enterprise", operation = "创建企业账号")
     public ResponseVO createAccount(@RequestBody CreateAccountDto createAccountDto) throws BusinessException {
         Integer type = createAccountDto.getType();
         if (!AccountTypeEnum.CITY.getCode().equals(type) && !AccountTypeEnum.ENTERPRISE.getCode().equals(type)) {
@@ -140,36 +142,71 @@ public class AccountController extends ABaseController {
         return getSuccessResponseVO(null);
     }
 
-    @GetMapping("/autoLogin")
-    public ResponseVO autoLogin(HttpServletRequest request, HttpServletResponse response) throws BusinessException {
-        String token = getTokenFromCookie(request);
-        if (StringUtils.isEmpty(token)) {
-            throw new BusinessException(ResponseCodeEnum.CODE_901);
-        }
-        if (redisComponent.getProvinceTokenInfo(token) == null) {
-            throw new BusinessException(ResponseCodeEnum.CODE_901);
-        }
-        try {
-            token = TokenUtils.generateToken();
-            saveToken2Cookie(response, token);
+    /**
+     * 加载所有企业列表
+     */
+    @GetMapping("/loadAllEnterprise")
+    public ResponseVO loadAllEnterprise(EnterpriseInfoQuery query) {
+        return getSuccessResponseVO(enterpriseInfoService.findListByPage(query));
+    }
 
-            redisComponent.saveProvinceTokenInfo(token);
+    /**
+     * 根据企业ID获取企业信息
+     */
+    @GetMapping("/getEnterprise/{enterpriseId}")
+    public ResponseVO getEnterprise(@PathVariable String enterpriseId) {
+        EnterpriseInfo enterpriseInfo = enterpriseInfoService.getEnterpriseInfoByEnterpriseId(enterpriseId);
+        return getSuccessResponseVO(enterpriseInfo);
+    }
 
-            return getSuccessResponseVO(null);
-        } finally {
-            // 清除旧的token
-            Cookie[] cookies = request.getCookies();
-            if (cookies != null) {
-                token = null;
-                for (Cookie cookie : cookies) {
-                    if (cookie.getName().equals(Constants.TOKEN_KEY)) {
-                        token = cookie.getValue();
-                    }
-                }
-                if (!StringUtils.isEmpty(token)) {
-                    redisComponent.cleanProvinceTokenInfo(token);
-                }
-            }
+    /**
+     * 更新企业信息
+     */
+    @PutMapping("/updateEnterprise/{enterpriseId}")
+    @OperationLog(module = "enterprise", operation = "更新企业信息")
+    public ResponseVO updateEnterprise(@PathVariable String enterpriseId, @RequestBody EnterpriseInfo enterpriseInfo) throws BusinessException {
+        enterpriseInfo.setUpdatedAt(new Date());
+        // 更新地区、性质、行业的一级分类代码
+        if(enterpriseInfo.getRegion() != null) {
+            enterpriseInfo.setRegionCode(com.yunnancommon.utils.StringTools.getTopCategoryCode(enterpriseInfo.getRegion(), com.yunnancommon.entity.constants.Constants.REGION));
         }
+        if(enterpriseInfo.getNature() != null) {
+            enterpriseInfo.setNatureCode(com.yunnancommon.utils.StringTools.getTopCategoryCode(enterpriseInfo.getNature(), com.yunnancommon.entity.constants.Constants.NATURE));
+        }
+        if(enterpriseInfo.getIndustry() != null) {
+            enterpriseInfo.setIndustryCode(com.yunnancommon.utils.StringTools.getTopCategoryCode(enterpriseInfo.getIndustry(), com.yunnancommon.entity.constants.Constants.INDUSTRY));
+        }
+        Integer result = enterpriseInfoService.updateEnterpriseInfoByEnterpriseId(enterpriseInfo, enterpriseId);
+        if (result == 0) {
+            throw new BusinessException("更新企业信息失败");
+        }
+        return getSuccessResponseVO(null);
+    }
+
+    /**
+     * 删除企业
+     */
+    @DeleteMapping("/deleteEnterprise/{enterpriseId}")
+    @OperationLog(module = "enterprise", operation = "删除企业")
+    public ResponseVO deleteEnterprise(@PathVariable String enterpriseId) throws BusinessException {
+        Integer result = enterpriseInfoService.deleteEnterpriseInfoByEnterpriseId(enterpriseId);
+        if (result == 0) {
+            throw new BusinessException("删除企业失败");
+        }
+        return getSuccessResponseVO(null);
+    }
+
+    /**
+     * 修改企业状态
+     */
+    @PostMapping("/changeEnterpriseStatus")
+    @OperationLog(module = "enterprise", operation = "修改企业状态")
+    public ResponseVO changeEnterpriseStatus(@RequestParam String enterpriseId,
+                                             @RequestParam Integer status) throws BusinessException {
+        Integer result = enterpriseInfoService.updateStatusByEnterpriseId(enterpriseId, status);
+        if (result == 0) {
+            throw new BusinessException("非法的企业状态流转或企业不存在");
+        }
+        return getSuccessResponseVO(null);
     }
 }
